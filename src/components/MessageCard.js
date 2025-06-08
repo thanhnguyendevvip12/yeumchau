0be5c9ad02abb4dffa4c31f6fc3b4bab8eaf9155
thanhnguyendevvip12 <haicon672009@gmail.com>
2025-06-08 20:59:56 +0700

    Update MessageCard.js

--- src/components/MessageCard.js
+++ src/components/MessageCard.js
@@ -181,12 +181,7 @@ const CustomHeart = ({ color }) => (
 );
 
 const messages = [
-    "Gửi đến người phụ nữ tuyệt vời nhất trong cuộc đời anh, em mang đến niềm vui và hạnh phúc cho mỗi ngày.",
-    "Nụ cười của em thắp sáng thế giới của anh, và tình yêu của em lấp đầy trái tim anh bằng sự ấm áp.",
-    "Vào ngày đặc biệt này, anh muốn em biết em có ý nghĩa như thế nào đối với anh.",
-    "Em không chỉ là bạn gái của anh, em là bạn thân nhất của anh, là người bạn tâm giao và là tất cả của anh.",
-    "Anh trân trọng từng khoảnh khắc chúng ta bên nhau, và anh mong muốn tạo ra nhiều kỷ niệm đẹp hơn nữa.",
-    "Tình yêu của anh! Em xứng đáng có được tất cả hạnh phúc trên thế giới này."
+    "Thành Yêu Châu Nhiều Lắm.",
 ];
 
 // Then update the MessageCard component to accept and pass the prop:
